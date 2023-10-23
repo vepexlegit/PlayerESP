ae1bf2da37d5b7d5f735257bca33e1b87ba07b52
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-10-23 21:38:25 +0400

    Create PlayerESP.java

---
+++ src/main/java/de/vepexlegit/playerespdebug/PlayerESP.java
@@ -0,0 +1,15 @@
+package de.vepexlegit.playerespdebug;
+
+public enum PlayerESP {
+    INSTANCE;
+
+    private boolean enabled = true;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
