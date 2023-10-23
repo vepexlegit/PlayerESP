813262c831e416743d36a7ef149f8ee001686550
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-10-23 21:39:05 +0400

    Create PlayerESPCommand.java

---
+++ src/main/java/de/vepexlegit/playerespdebug/commands/PlayerESPCommand.java
@@ -0,0 +1,56 @@
+package de.vepexlegit.playerespdebug.commands;
+
+import de.vepexlegit.playerespdebug.PlayerESP;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentText;
+import java.util.Collections;
+import java.util.List;
+
+public class PlayerESPCommand implements ICommand {
+    @Override
+    public String getCommandName() {
+        return "playeresp";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return null;
+    }
+
+    @Override
+    public List<String> getCommandAliases() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) {
+        if (args.length != 1 || (!args[0].equalsIgnoreCase("off") && !args[0].equalsIgnoreCase("on"))) {
+            sender.addChatMessage(new ChatComponentText("Invalid Arguments."));
+            return;
+        }
+        PlayerESP.INSTANCE.setEnabled(args[0].equalsIgnoreCase("on"));
+        sender.addChatMessage(new ChatComponentText(String.format("PlayerESP has been %s.", PlayerESP.INSTANCE.isEnabled() ? "enabled" : "disabled")));
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+
+    @Override
+    public int compareTo(ICommand o) {
+        return 0;
+    }
+}
